05c227fd89ab968c70408b5b07c3afca6e442dbb
Drenovaa <guilherme_silva_123@hotmail.com>
2025-07-25 15:20:54 -0300

    Criando logica do jogo e adicionando readme

---
+++ challenge-amigo-secreto/Readme.md
@@ -0,0 +1,10 @@
+### Desafio amigo secreto
+
+### Como funciona
+- Na pagina você adiciona os nomes das pessoas que são inseridas no jogo
+- Deve ser colocado um nome por vez
+- Após inserir todos os nomes clique no botão sortear amigo secreto
+- Você pode tentar sortear novamente outro nome clinado outra vez no mesmo botão
+
+### O que não funciona
+- Muita coisa no mundo não funciona, mas a App deve ter menos problemas que o resto do mundo
--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -1,1 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo () {
+    let nomeDoAmigo = document.querySelector('input').value;
+
+    if (nomeDoAmigo == "") {
+        alert("O input do nome não pode ser vazio");
+        return;
+    } else if (listaDeAmigos.includes(nomeDoAmigo)) {
+        alert("Já existe uma pessoa com o mesmo nome na lista")
+        return;
+    } else{
+        listaDeAmigos.push(nomeDoAmigo);
+        exibirListaDeAmigos()
+        return nomeDoAmigo
+    }
+};
+
+function exibirListaDeAmigos () {
+    let textoFormatado = listaDeAmigos.join(", ")
+    exibirTexto("listaAmigos", `${textoFormatado}`);
+}
+
+function exibirAmigoSorteado (id) {
+    exibirTexto("resultado", `O amigo secreto sorteado é: ${listaDeAmigos[id]}`);
+}
+
+function exibirTexto(id, texto){
+    let campo = document.getElementById(id);
+    campo.innerHTML = texto;
+};
+
+function sortearAmigo() {
+    let tamanhoDaLista = listaDeAmigos.length;
+    let amigoSorteado = parseInt(Math.random() * tamanhoDaLista);
+    return exibirAmigoSorteado(amigoSorteado)
+};
